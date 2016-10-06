f95c85bb195e15510c197d872a00f533f40ea364
KANHAIYA <kanhaiya.1609@gmail.com>
2016-10-07 01:29:13 +0530

    Added review ratings to stars

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :reviews
-  devise_for :users
-  resources :movies
 
-  root to: "movies#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 	devise_for :users
+
+ 	resources :movies do
+  	  resources :reviews, except: [:show, :index]
+ 	end
+
+ 	root to: "movies#index"
+ 	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
